e06a7282be538b106c9d43d8b4dc752f9a49deb9
caroAM22 <caalvarezmu@unal.edu.co>
2025-07-19 10:44:54 -0500

    Fix: type id

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -1,29 +1,29 @@
 -- Tabla Categoria
 IF NOT EXISTS CREATE TABLE categoria (
-    id VARCHAR(36) PRIMARY KEY,
+    id CHAR(36) PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     descripcion TEXT
 );
 
 -- Tabla Restaurantes
 IF NOT EXISTS CREATE TABLE restaurantes (
-    id VARCHAR(36) PRIMARY KEY,
+    id CHAR(36) PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     direccion VARCHAR(200) NOT NULL,
-    id_propietario VARCHAR(36) NOT NULL, -- FK a tabla users (ms-users)
-    telefono VARCHAR(20) NOT NULL,
-    url_logo VARCHAR(500),
+    id_propietario CHAR(36) NOT NULL, -- FK a tabla users (ms-users)
+    telefono VARCHAR(13) NOT NULL,
+    url_logo VARCHAR(500) NOT NULL,
     nit VARCHAR(20) NOT NULL UNIQUE
 );
 
 -- Tabla Platos
 IF NOT EXISTS CREATE TABLE platos (
-    id VARCHAR(36) PRIMARY KEY,
+    id CHAR(36) PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
-    id_categoria VARCHAR(36) NOT NULL,
+    id_categoria CHAR(36) NOT NULL,
     descripcion TEXT,
     precio DECIMAL(10,2) NOT NULL,
-    id_restaurante VARCHAR(36) NOT NULL,
+    id_restaurante CHAR(36) NOT NULL,
     url_imagen VARCHAR(500),
     activo BOOLEAN DEFAULT TRUE,
     FOREIGN KEY (id_categoria) REFERENCES categoria(id),
@@ -32,19 +32,19 @@ IF NOT EXISTS CREATE TABLE platos (
 
 -- Tabla Pedidos
 IF NOT EXISTS CREATE TABLE pedidos (
-    id VARCHAR(36) PRIMARY KEY,
-    id_cliente VARCHAR(36) NOT NULL, -- FK a tabla users (ms-users)
+    id CHAR(36) PRIMARY KEY,
+    id_cliente CHAR(36) NOT NULL, -- FK a tabla users (ms-users)
     fecha DATETIME NOT NULL,
     estado VARCHAR(50) NOT NULL,
-    id_chef VARCHAR(36), -- FK a tabla users (ms-users)
-    id_restaurante VARCHAR(36) NOT NULL,
+    id_chef CHAR(36), -- FK a tabla users (ms-users)
+    id_restaurante CHAR(36) NOT NULL,
     FOREIGN KEY (id_restaurante) REFERENCES restaurantes(id)
 );
 
 -- Tabla Pedidos_Platos (tabla de relación)
 IF NOT EXISTS CREATE TABLE pedidos_platos (
-    id_pedido VARCHAR(36) NOT NULL,
-    id_plato VARCHAR(36) NOT NULL,
+    id_pedido CHAR(36) NOT NULL,
+    id_plato CHAR(36) NOT NULL,
     cantidad INT NOT NULL,
     PRIMARY KEY (id_pedido, id_plato),
     FOREIGN KEY (id_pedido) REFERENCES pedidos(id),
